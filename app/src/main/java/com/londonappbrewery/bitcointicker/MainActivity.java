1b4e55b84fa3591a365a6105cfb4973a04c982c7
jcloud <splendidseacollection@gmail.com>
2018-04-30 12:31:36 -0400

    Bitcoin tracker app "Am I Rich?" app completed!

--- app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
@@ -12,19 +12,27 @@ import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Toast;
 
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import cz.msebera.android.httpclient.Header;
 
 
 public class MainActivity extends AppCompatActivity {
 
     // Constants:
     // TODO: Create the base URL
-    private final String BASE_URL = "https://apiv2.bitcoin ...";
+    private final String BASE_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC";
+    //Final int REQUEST_CODE = 123;
 
     // Member Variables:
     TextView mPriceTextView;
+    String mLast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,34 +54,84 @@ public class MainActivity extends AppCompatActivity {
 
         // TODO: Set an OnItemSelected listener on the spinner
 
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            parent.getItemAtPosition(position);
+                 Log.d("Bitcoin", "You are selecting " + parent.getItemAtPosition(position));
+                 Log.d("Bitcoin", "Position is: " + position);
+
+                 String finalUrl = BASE_URL + parent.getItemAtPosition(position);
+                 Log.d("Bitcoin", BASE_URL + parent.getItemAtPosition(position));
+                 letsDoSomeNetworking(finalUrl); /* the contents of the () changed from 'BASE_URL' to 'finalUrl' */
+
+            /* ^^ THIS IS IT, you are "CALLING" a method/object with the parameters defined to
+                  'get' the correct information*/
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+                Log.d("Bitcoin", "Nothing Selected");
+
+            }
+        });
+
     }
+
+
+
+    // just messing around with the code below, trying to figure out parameters for the get request
+
+   /* private void getWeatherForNewCity (String city) {
+        RequestParams params = new RequestParams();
+        params.put("q", city);
+        letsDoSomeNetworking(params);
+    not relevant since letsDoSomeNetworking is not using 'params' but 'String url' for this example}*/
+
+
+
+
 
     // TODO: complete the letsDoSomeNetworking() method
     private void letsDoSomeNetworking(String url) {
 
-//        AsyncHttpClient client = new AsyncHttpClient();
-//        client.get(WEATHER_URL, params, new JsonHttpResponseHandler() {
-//
-//            @Override
-//            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-//                // called when response HTTP status is "200 OK"
-//                Log.d("Clima", "JSON: " + response.toString());
-//                WeatherDataModel weatherData = WeatherDataModel.fromJson(response);
-//                updateUI(weatherData);
-//            }
-//
-//            @Override
-//            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
-//                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-//                Log.d("Clima", "Request fail! Status code: " + statusCode);
-//                Log.d("Clima", "Fail response: " + response);
-//                Log.e("ERROR", e.toString());
-//                Toast.makeText(WeatherController.this, "Request Failed", Toast.LENGTH_SHORT).show();
-//            }
-//        });
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(url, new JsonHttpResponseHandler() {   /*BASE_URL needs to change to just url*/
 
 
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // called when response HTTP status is "200 OK"
+                Log.d("Bitcoin", "JSON: " + response.toString());
+
+                try {
+                    mLast = response.getString("last");
+                    mPriceTextView.setText(mLast);
+                }
+
+                catch (JSONException e1) {
+                    e1.printStackTrace();
+
+                }
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                Log.d("Bitcoin", "Request fail! Status code: " + statusCode);
+                Log.d("Bitcoin", "Fail response: " + response);
+                Log.e("ERROR", e.toString());
+                Toast.makeText(MainActivity.this, "Request Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        }
     }
 
 
-}
+
+
